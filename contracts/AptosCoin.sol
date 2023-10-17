d02f443d1f134999d539eb02812c05b7e497bce1
Niket Yende <niket6@gmail.com>
2023-10-17 18:43:51 +0530

    Add AptosCoin contract

---
+++ contracts/AptosCoin.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract AptosCoin is ERC20, ERC20Burnable {
+    address public owner;
+
+    modifier onlyOwner {
+        require(owner == msg.sender, "Only owner");
+        _;
+    }
+
+    constructor(uint256 amount) ERC20("AptosCoin", "APT") {
+        owner = msg.sender;
+        mint(msg.sender, amount);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
